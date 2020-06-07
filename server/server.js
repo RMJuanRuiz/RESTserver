931f02f344ad570ea7a57f2fff20137c82801557
Juan <juanpablo310399@gmail.com>
2020-06-07 18:49:33 -0500

    Initial request configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/user', function(req, res) {
+    res.json('get user');
 })
 
-app.listen(8080, () => {
-    console.log('listening 8080 port');
+app.post('/user', function(req, res) {
+    let body = req.body;
+
+    if (body.name === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'The name is necessary'
+        });
+
+    } else {
+        res.json({
+            person: body
+        });
+    }
+})
+
+app.put('/user/:id', function(req, res) {
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+})
+
+app.delete('/user', function(req, res) {
+    res.json('delete user');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening port ${ process.env.PORT }`);
 })
